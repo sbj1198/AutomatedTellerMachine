1c33533592369869785c4c685a0b3174c8dd3e8d
sbj1198 <sbhaskar1998@gmail.com>
2023-05-02 18:59:53 +0530

    Added: actions to HomeController & modified the RouteConfig

--- AutomatedTellerMachine/App_Start/RouteConfig.cs
+++ AutomatedTellerMachine/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace AutomatedTellerMachine
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Serial",
+                url: "serial/{letterCase}",
+                defaults: new { controller = "Home", action = "Serial", letterCase = "upper" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- AutomatedTellerMachine/Controllers/HomeController.cs
+++ AutomatedTellerMachine/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace AutomatedTellerMachine.Controllers
@@ -22,9 +24,31 @@ namespace AutomatedTellerMachine.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.TheMessage = "Having trouble? Send us a message.";
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.TheMessage = "Thanks, we got your message!";
+
+            return View();
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ASPNETMVC5ATM1";
+            if(letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            //return Content(serial);
+            //return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+            return RedirectToAction("Index");
+        }
     }
 }
+
+//< compiler language = "c#;cs;csharp" extension = ".cs" warningLevel = "4" compilerOptions = "/langversion:default /nowarn:1659;1699;1701;612;618" type = "Microsoft.CodeDom.Providers.DotNetCompilerPlatform.CSharpCodeProvider, Microsoft.CodeDom.Providers.DotNetCompilerPlatform, Version=4.1.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" />
