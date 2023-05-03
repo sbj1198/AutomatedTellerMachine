87e9a5902b6666a3e45d634d12225a9eee22bceb
sbj1198 <sbhaskar1998@gmail.com>
2023-05-03 18:02:54 +0530

    Added: CheckingAccount  Model, CheckingAccount controller

---
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+        
+        [Display(Name ="Account #")]
+        public string AccountNumber { get; set; }
+
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+    }
+}
--- AutomatedTellerMachine/MyLoggingFilter.cs
+++ AutomatedTellerMachine/MyLoggingFilter.cs
@@ -11,7 +11,7 @@ namespace AutomatedTellerMachine
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
             var request = filterContext.HttpContext.Request;
-            //Logger.logRequest(request.UserHostAddress);
+            //Logger.logRequest(request.UserHostAddress); 
             base.OnActionExecuted(filterContext);
         }
     }
